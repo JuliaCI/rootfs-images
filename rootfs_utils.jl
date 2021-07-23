9d05f92c5d9fc478f948d764ce0d8944dca032d1
Dilum Aluthge <dilum@aluthge.com>
2021-07-23 03:42:46 -0400

    Fix a typo in a variable name inside the `_is_github_actions_event` function

--- rootfs_utils.jl
+++ rootfs_utils.jl
@@ -219,8 +219,8 @@ function is_github_actions()
 end
 function _is_github_actions_event(event_name::AbstractString)
     is_gha = is_github_actions()
-    is_pr = get(ENV, "GITHUB_EVENT_NAME", "") == event_name
-    return is_gha && is_pr
+    is_event = get(ENV, "GITHUB_EVENT_NAME", "") == event_name
+    return is_gha && is_event
 end
 function is_github_actions_pr()
     return _is_github_actions_event("pull_request")
