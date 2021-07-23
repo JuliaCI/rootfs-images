0ea62b345eaa2242eb0a1fb93523c8dabefaceab
Dilum Aluthge <dilum@aluthge.com>
2021-07-22 21:42:45 -0400

    Reorganize the files, and put all of the rootfs image definitions into the `images` subdirectory (#8)

--- images/llvm_passes.jl
+++ images/llvm_passes.jl
@@ -2,7 +2,7 @@
 ## Eventually, this image will probably be replaced with the actual builder image,
 ## as that will have the necessary toolchains as well, but that image is not built yet.
 
-include("rootfs_utils.jl")
+include(joinpath(dirname(@__DIR__), "rootfs_utils.jl"))
 
 # Build debian-based image with the following extra packages:
 const packages = [
