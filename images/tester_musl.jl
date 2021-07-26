7d5e84ace703a2143a0bcb987009b57a1eef588b
Dilum Aluthge <dilum@aluthge.com>
2021-07-25 23:32:57 -0400

    Add the `tester_musl.x86_64` rootfs image, which does not include the compiler toolchain

--- images/tester_musl.jl
+++ images/tester_musl.jl
@@ -4,14 +4,14 @@ include(joinpath(dirname(@__DIR__), "rootfs_utils.jl"))
 arch, = parse_args(ARGS)
 image = "$(splitext(basename(@__FILE__))[1]).$(arch)"
 
-# Build debian-based image with the following extra packages:
+# Build alpine-based image with the following extra packages:
 packages = [
-    "bash",
-    "curl",
-    "gdb",
-    "vim",
+    AlpinePackage("bash"),
+    AlpinePackage("curl"),
+    AlpinePackage("gdb"),
+    AlpinePackage("vim"),
 ]
-tarball_path = debootstrap(arch, image; packages)
+tarball_path = alpine_bootstrap(image; packages)
 
 # Upload it
 upload_rootfs_image_github_actions(tarball_path)
