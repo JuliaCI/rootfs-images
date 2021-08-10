6cbed4cfca7b5fe2171303e4aa2670214a09281a
Dilum Aluthge <dilum@aluthge.com>
2021-08-09 23:02:58 -0400

    Use ArgParse.jl to parse the command-line args for the `test_rootfs.jl` script

--- src/build/args.jl
+++ src/build/args.jl
@@ -1,10 +1,19 @@
-function parse_build_args(args::AbstractVector)
-    settings = ArgParse.ArgParseSettings()
+function generate_image_name(arch::AbstractString, file::AbstractString)
+    return "$(splitext(basename(file))[1]).$(arch)"
+end
+
+function parse_build_args(args::AbstractVector, file::AbstractString)
+    settings = ArgParse.ArgParseSettings(;
+        description = "Build a rootfs image",
+    )
     ArgParse.@add_arg_table! settings begin
         "--arch"
-            required=true
+            arg_type = String
+            required = true
+            help = "The architecture for which you would like to build"
     end
     parsed_args = ArgParse.parse_args(args, settings)
     arch = parsed_args["arch"]::String
-    return (; arch)
+    image = generate_image_name(arch, file)
+    return (; arch, image)
 end
--- test_rootfs.jl
+++ test_rootfs.jl
@@ -1,36 +1,14 @@
-#!/usr/bin/env julia
-using Sandbox, Pkg.Artifacts
+using Pkg.Artifacts
+using RootfsUtils
+using Sandbox
 
-usage_msg = "Usage: $(basename(@__FILE__)) <url> [gitsha] [command...]"
-
-isempty(ARGS) && throw(ArgumentError(usage_msg))
-
-url = strip(popfirst!(ARGS))
-if isempty(url)
-    url = nothing
-end
-if isempty(ARGS)
-    @warn("hash not provided; this will download the tarball, then fail, so you can see the true hash")
-    hash = Base.SHA1("0000000000000000000000000000000000000000")
-else
-    hash = Base.SHA1(strip(popfirst!(ARGS)))
-end
-if isempty(ARGS)
-    cmd = `/bin/bash`
-else
-    cmd = `$ARGS`
-end
+command, treehash, url, = parse_test_args(ARGS, @__FILE__)
 
 # If the artifact is not locally existent, download it
-if !artifact_exists(hash)
-    @info("Artifact did not exist, downloading")
-    url === nothing && throw(ArgumentError(usage_msg))
-    was_success = download_artifact(hash, url; verbose=true)
-    was_success || throw(ErrorException("Download was not a success"))
-end
+ensure_artifact_exists_locally(; treehash, url)
 
 config = SandboxConfig(
-    Dict("/" => artifact_path(hash)),
+    Dict("/" => artifact_path(treehash)),
     Dict{String,String}(),
     Dict(
         "PATH" => "/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin",
@@ -45,5 +23,5 @@ config = SandboxConfig(
 )
 
 with_executor() do exe
-    run(exe, config, cmd)
+    run(exe, config, command)
 end
