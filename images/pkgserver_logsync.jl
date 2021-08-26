7cfe843354873c80b370a450dfe401fd087056df
Elliot Saba <staticfloat@gmail.com>
2021-08-26 11:34:19 -0700

    Add `pkgserver_logsync` rootfs image (#92)
    This is the image used in the PkgServer log analysis buildkite pipeline
    that runs from this pipeline yaml [0].
    
    [0] https://github.com/JuliaPackaging/PkgServerLogAnalysis.jl/blob/master/.buildkite/s3_sync_job.yml

---
+++ images/pkgserver_logsync.jl
@@ -0,0 +1,21 @@
+using RootfsUtils
+
+arch, image, = parse_build_args(ARGS, @__FILE__)
+
+# Build debian-based image with the following extra packages:
+packages = String[
+    "openssh-client",
+    "awscli",
+    "rsync",
+    "curl",
+    "jq",
+    "zstd",
+]
+artifact_hash, tarball_path, = debootstrap(arch, image; packages)
+
+# Upload it
+upload_rootfs_image_github_actions(tarball_path)
+
+# Test that we can use our new rootfs image with Sandbox.jl
+test_sandbox(artifact_hash)
+
