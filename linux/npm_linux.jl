7b020402b2e0e626b335ade2798b4ed011bc2af8
anand jain <anandj@uchicago.edu>
2021-10-07 13:33:53 -0400

    rudimentary npm rootfs builder

---
+++ linux/npm_linux.jl
@@ -0,0 +1,21 @@
+using RootfsUtils: parse_build_args, debootstrap, chroot, upload_gha, test_sandbox
+
+args         = parse_build_args(ARGS, @__FILE__)
+arch         = args.arch
+archive      = args.archive
+image        = args.image
+
+packages = [
+    "apt-transport-https",
+    "curl",
+    "git",
+    "locales",
+    "vim",
+    "npm",
+    "wget",
+]
+
+artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages) 
+
+upload_gha(tarball_path)
+test_sandbox(artifact_hash)
