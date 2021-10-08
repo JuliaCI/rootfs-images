b733dd86327e9567fa3e867ce4832d03d017741a
Elliot Saba <staticfloat@gmail.com>
2021-10-07 21:51:14 -0700

    Add `python_latex_kitchen_sink` image (#121)

---
+++ linux/python_latex_kitchen_sink.jl
@@ -0,0 +1,38 @@
+# This image is a bit of an anomaly; we install pdflatex, python3.... pretty much the kitchen sink
+# It is used by pipelines that use the SciML ecosystem such as the SciMLBenchmarks repository.
+using RootfsUtils: parse_build_args, debootstrap, chroot, upload_gha, test_sandbox
+
+args         = parse_build_args(ARGS, @__FILE__)
+arch         = args.arch
+archive      = args.archive
+image        = args.image
+
+packages = [
+    "bash",
+    "locales",
+    
+    # Work around bug in debootstrap where virtual dependencies are not properly installed
+    # X-ref: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=878961
+    # X-ref: https://bugs.launchpad.net/ubuntu/+source/debootstrap/+bug/86536
+    "perl-openssl-defaults",
+    "dbus-user-session",
+    "python3-sip",
+
+
+    # Get a C compiler, for compiling python extensions
+    "build-essential",
+    # Get latex, so that we can invoke `pdflatex` and friends
+    "texlive-full",
+    # Some of our packages require PyCall.jl/Conda.jl deps
+    "python3",
+    "liblapack3",
+
+    # These are just for debugging
+    "curl",
+    "vim",
+    "gdb",
+]
+
+artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages)
+upload_gha(tarball_path)
+test_sandbox(artifact_hash)
