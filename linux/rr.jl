0efbf43fd9ab19a67711433a46e4d4fe9bfe440c
Dilum Aluthge <dilum@aluthge.com>
2022-01-20 00:21:15 -0500

    Install CMake 3.21+ in the `rr.x86_64` image (#155)

--- linux/rr.jl
+++ linux/rr.jl
@@ -1,5 +1,6 @@
 using RootfsUtils: parse_build_args, upload_gha, test_sandbox
 using RootfsUtils: debootstrap
+using RootfsUtils: root_chroot
 
 args         = parse_build_args(ARGS, @__FILE__)
 arch         = args.arch
@@ -11,7 +12,6 @@ packages = [
     "build-essential",
     "capnproto",
     "ccache",
-    "cmake",
     "coreutils",
     "curl",
     "g++-multilib",
@@ -27,6 +27,21 @@ packages = [
     "vim",
 ]
 
-artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages)
+artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages) do rootfs, chroot_ENV
+    my_chroot(args...) = root_chroot(args...; ENV=chroot_ENV)
+
+    # We need cmake 3.21+ for the `--output-xunit` feature
+    @info("Installing cmake")
+    cmake_install_cmd = """
+    echo 'deb http://deb.debian.org/debian testing main' >> /etc/apt/sources.list && \\
+    apt-get update && \\
+    DEBIAN_FRONTEND=noninteractive apt-get install -y cmake
+    """
+    my_chroot(rootfs, "bash", "-c", cmake_install_cmd)
+    my_chroot(rootfs, "bash", "-c", "which cmake")
+    my_chroot(rootfs, "bash", "-c", "which -a cmake")
+    my_chroot(rootfs, "bash", "-c", "cmake --version")
+end
+
 upload_gha(tarball_path)
 test_sandbox(artifact_hash)
