c3bc9c948f9c74ddf626e100cfa3c37a4878e56d
Elliot Saba <staticfloat@gmail.com>
2022-04-20 23:43:07 -0700

    Update i686-linux-gnu packager

--- linux/package_linux.jl
+++ linux/package_linux.jl
@@ -37,22 +37,22 @@ packages = [
 artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages) do rootfs, chroot_ENV
     my_chroot(args...) = root_chroot(rootfs, "bash", "-c", args...; ENV=chroot_ENV)
 
-    if arch == "x86_64"
-        host_triplet = "x86_64-linux-gnu"
+    if arch ∈ ("x86_64", "i686")
+        host_triplet = "$(arch)-linux-gnu"
         # Install GCC 9 from Elliot's repo
-        repo_release_url = "https://github.com/staticfloat/linux-gcc-toolchains/releases/download/GCC-v9.1.0-x86_64-linux-gnu"
+        repo_release_url = "https://github.com/staticfloat/linux-gcc-toolchains/releases/download/GCC-v9.1.0-$(host_triplet)"
         gcc_install_cmd = """
         cd /usr/local
-        curl -L $(repo_release_url)/GCC.v9.1.0.$(host_triplet)-target_libc+glibc-target_os+linux-target_arch+x86_64.tar.gz | tar zx
-        curl -L $(repo_release_url)/Binutils.v2.38.0.x86_64-linux-gnu-target_libc+glibc-target_os+linux-target_arch+x86_64.tar.gz | tar zx
+        curl -L $(repo_release_url)/GCC.v9.1.0.$(host_triplet)-target_libc+glibc-target_os+linux-target_arch+$(arch).tar.gz | tar zx
+        curl -L $(repo_release_url)/Binutils.v2.38.0.x86_64-linux-gnu-target_libc+glibc-target_os+linux-target_arch+$(arch).tar.gz | tar zx
         curl -L $(repo_release_url)/Zlib.v1.2.12.$(host_triplet).tar.gz | tar zx
         cd /usr/local/$(host_triplet)/
         curl -L $(repo_release_url)/Glibc.v2.12.2.$(host_triplet).tar.gz | tar zx
         cd /usr/local/x86_64-linux-gnu/usr
-        curl -L $(repo_release_url)/LinuxKernelHeaders.v5.15.14.x86_64-linux-target_os+linux-target_arch+x86_64.tar.gz | tar zx
+        curl -L $(repo_release_url)/LinuxKernelHeaders.v5.15.14.x86_64-linux-target_os+linux-target_arch+$(arch).tar.gz | tar zx
         """
         gcc_symlink_cmd = """
-        # Create symlinks for `gcc` -> `x86_64-linux-gnu-gcc`, etc...
+        # Create symlinks for `gcc` -> `$(host_triplet)-gcc`, etc...
         for tool_path in /usr/local/bin/$(host_triplet)-*; do
             tool="\$(basename "\${tool_path}" | sed -e 's/$(host_triplet)-//')"
             ln -sf "$(host_triplet)-\${tool}" "/usr/local/bin/\${tool}"
