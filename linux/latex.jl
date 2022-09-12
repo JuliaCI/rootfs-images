3c78e504f2399689159843631a854470b3e98e45
Dilum Aluthge <dilum@aluthge.com>
2022-09-12 01:26:51 -0400

    Add the `latex.x86_64` image for building the PDF docs on Buildkite (#224)

---
+++ linux/latex.jl
@@ -0,0 +1,39 @@
+using RootfsUtils: parse_build_args, upload_gha, test_sandbox
+using RootfsUtils: debootstrap
+using RootfsUtils: root_chroot
+
+args         = parse_build_args(ARGS, @__FILE__)
+arch         = args.arch
+archive      = args.archive
+image        = args.image
+
+packages = [
+    "bash",
+    "locales",
+    "localepurge",
+]
+
+artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages) do rootfs, chroot_ENV
+    my_chroot(args...) = root_chroot(rootfs, "bash", "-eu", "-o", "pipefail", "-c", args...; ENV=chroot_ENV)
+
+    additional_packages = String[
+        "curl",
+        "latexmk",
+        "python3-pygments",
+        "texlive-latex-base",
+        "texlive-latex-extra",
+        "texlive-latex-recommended",
+        "texlive-luatex",
+    ]
+    additional_packages_string = join(additional_packages, " ")
+    cmd = """
+    set -Eeu -o pipefail
+    apt-get update
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends $(additional_packages_string)
+    rm -rf /var/lib/apt/lists/*
+    """
+    my_chroot(cmd)
+end
+
+upload_gha(tarball_path)
+test_sandbox(artifact_hash)
