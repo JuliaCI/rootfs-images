e63891c79bab9509caa2730c7130ebde5b572723
Dilum Aluthge <dilum@aluthge.com>
2022-10-19 00:01:04 -0400

    Add the `xvfb` rootfs image (#236)
    * Add the `xvfb` rootfs image
    
    * Alphabetize the list of rootfs images

---
+++ linux/xvfb.jl
@@ -0,0 +1,17 @@
+using RootfsUtils: parse_build_args, upload_gha, test_sandbox
+using RootfsUtils: debootstrap
+
+args         = parse_build_args(ARGS, @__FILE__)
+arch         = args.arch
+archive      = args.archive
+image        = args.image
+
+packages = [
+    "xvfb",
+    "locales",
+    "localepurge",
+]
+
+artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages)
+upload_gha(tarball_path)
+test_sandbox(artifact_hash)
