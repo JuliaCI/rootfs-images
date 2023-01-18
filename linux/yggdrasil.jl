601ac3d385ccf26019aabd17ff82bc01c9493559
Dilum Aluthge <dilum@aluthge.com>
2023-01-18 08:44:31 -0500

    Create a separate rootfs image for Yggdrasil

---
+++ linux/yggdrasil.jl
@@ -0,0 +1,37 @@
+using RootfsUtils: parse_build_args, upload_gha, test_sandbox
+using RootfsUtils: debootstrap
+using RootfsUtils: root_chroot
+
+args         = parse_build_args(ARGS, @__FILE__)
+arch         = args.arch
+archive      = args.archive
+image        = args.image
+
+packages = [
+    "apt-transport-https",
+    "bzip2",
+    "curl",
+    "expect",
+    "git",
+    "gnupg2",
+    "iproute2",
+    "jq",
+    "libgomp1",
+    "libicu63",
+    "localepurge",
+    "locales",
+    "openssh-client",
+    "openssl",
+    "p7zip",
+    "python3",
+    "ssh",
+    "unzip",
+    "vim",
+    "wget",
+    "xz-utils",
+    "zstd",
+]
+
+artifact_hash, tarball_path, = debootstrap(arch, image; archive, packages, locales)
+upload_gha(tarball_path)
+test_sandbox(artifact_hash)
